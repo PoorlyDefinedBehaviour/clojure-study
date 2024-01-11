3e53cbbb2308cb7e081797039db93b4bfd3d87fa
PoorlyDefinedBehaviour <brunotj2015@hotmail.com>
2024-01-10 21:09:25 -0300

    composing transducers

--- chapter-4/eager.clj
+++ chapter-4/eager.clj
@@ -11,3 +11,21 @@
 
 (sum-squares 5)
 ;; [0 1 4 9 16]
+
+(require '[clojure.string])
+
+(defn preds-seq []
+  (->> (all-ns)
+       (map ns-publics)
+       (mapcat vals)
+       (filter #(clojure.string/ends-with? % "?"))
+       (map #(str (.-sym %)))
+       vec))
+
+(defn preds []
+  (into []
+        (comp (map ns-publics)
+              (mapcat vals)
+              (filter #(clojure.string/ends-with? % "?"))
+              (map #(str (.-sym %))))
+        (all-ns)))
