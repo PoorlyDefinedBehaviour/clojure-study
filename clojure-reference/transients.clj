07e123e670f883aab1662561f550f19a3a0c01e9
PoorlyDefinedBehaviour <brunotj2015@hotmail.com>
2025-06-22 11:40:50 -0300

    clojure ref: transients

---
+++ clojure-reference/transients.clj
@@ -0,0 +1,15 @@
+(defn vrange [n]
+  (loop [i 0 v []]
+    (if (< i n)
+      (recur (inc i) (conj v i))
+      v)))
+
+(vrange 5)
+
+(defn vrange2 [n]
+  (loop [i 0 v (transient [])]
+    (if (< i n)
+      (recur (inc i) (conj! v i))
+      (persistent! v))))
+
+(vrange2 5)
